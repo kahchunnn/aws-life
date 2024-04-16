3394ab7e8c50c216a3d87b8a44c684130dc9d09a
kahchunnn <167161139+kahchunnn@users.noreply.github.com>
2024-04-16 12:53:17 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cl8o0ck6ejzi.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "limkahchun-employee"
+customregion = "us-east-1"
+
